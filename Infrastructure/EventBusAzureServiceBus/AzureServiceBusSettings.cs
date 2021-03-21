3004cd5430cf6daf12fd7bfc81e07b402eb282b0
Alejandro de Arriba <adearriba@outlook.com>
2021-03-21 13:09:28 +0100

    Create AzureServiceBusSettings.cs

---
+++ Infrastructure/EventBusAzureServiceBus/AzureServiceBusSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.AzureServiceBus
+{
+    public record AzureServiceBusSettings
+    {
+        public static AzureServiceBusSettings _instance = null;
+
+        public string IntegrationEventSuffix { get; init; }
+
+        private AzureServiceBusSettings()
+        {
+            IntegrationEventSuffix = Environment.GetEnvironmentVariable("IntegrationEventSuffix") ?? "IntegrationEvent";
+        }
+
+        public static AzureServiceBusSettings GetInstance()
+        {
+            if (_instance != null) return _instance;
+
+            _instance = new AzureServiceBusSettings();
+            return _instance;
+        }
+    }
+}
