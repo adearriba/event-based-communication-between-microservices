41ce2eb5fb1bf97e91b4fdaef0276e15503063d8
Alejandro de Arriba <adearriba@outlook.com>
2021-03-21 13:09:50 +0100

    Created Azure Persistent connection interface and implementation

---
+++ Infrastructure/EventBusAzureServiceBus/Connections/AzureServiceBusPersistentConnection.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+
+namespace EventBus.AzureServiceBus.Connections
+{
+    public class AzureServiceBusPersistentConnection : IServiceBusPersistentConnection
+    {
+        private readonly ServiceBusConnectionStringBuilder _serviceBusConnectionStringBuilder;
+        private readonly string _subscriptionClientName;
+        private ISubscriptionClient _subscriptionClient;
+        private ITopicClient _topicClient;
+        private bool _disposed;
+
+        public AzureServiceBusPersistentConnection(ServiceBusConnectionStringBuilder serviceBusConnectionStringBuilder,
+            string subscriptionClientName)
+        {
+            _serviceBusConnectionStringBuilder = serviceBusConnectionStringBuilder ??
+                throw new ArgumentNullException(nameof(serviceBusConnectionStringBuilder));
+            _subscriptionClientName = subscriptionClientName;
+
+            _subscriptionClient = new SubscriptionClient(_serviceBusConnectionStringBuilder, _subscriptionClientName);
+            _topicClient = new TopicClient(_serviceBusConnectionStringBuilder, RetryPolicy.Default);
+        }
+
+        public ITopicClient TopicClient
+        {
+            get
+            {
+                if (_topicClient.IsClosedOrClosing)
+                {
+                    _topicClient = new TopicClient(_serviceBusConnectionStringBuilder, RetryPolicy.Default);
+                }
+                return _topicClient;
+            }
+        }
+
+        public ISubscriptionClient SubscriptionClient
+        {
+            get
+            {
+                if (_subscriptionClient.IsClosedOrClosing)
+                {
+                    _subscriptionClient = new SubscriptionClient(_serviceBusConnectionStringBuilder, _subscriptionClientName);
+                }
+                return _subscriptionClient;
+            }
+        }
+
+        public ServiceBusConnectionStringBuilder ServiceBusConnectionStringBuilder => _serviceBusConnectionStringBuilder;
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _disposed = true;
+        }
+    }
+}
---
+++ Infrastructure/EventBusAzureServiceBus/Connections/IServiceBusPersistentConnection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+
+namespace EventBus.AzureServiceBus.Connections
+{
+    public interface IServiceBusPersistentConnection : IDisposable
+    {
+        ITopicClient TopicClient { get; }
+        ISubscriptionClient SubscriptionClient { get; }
+    }
+}
